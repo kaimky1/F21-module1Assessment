40514f608794bf2a938572f251d39adf8f6ed6eb
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-03-25 07:08:56 -1000

    second commit

--- password.js
+++ password.js
@@ -9,16 +9,13 @@ const reader = readline.createInterface({
 
 reader.question("Enter a password to validate:", function(input){
 	tokens = input.split(' ');
-	
 	password = String(tokens[0]);
-	num1 = Number(tokens[1]);
-	num2 = Number(tokens[2]);
-
 	if (password.length >= 10){
 		console.log("Success!")
 	}else{
-		console.log("Your password needs to be at least 10 characters.")
+		console.log("Your password needs to be at least 10 characters and contain one uppercase character.")
 	}
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
