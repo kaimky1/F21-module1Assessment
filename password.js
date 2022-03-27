bdef0f8506c3c8c0ea8834fd5504dfb16b0e01d4
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-03-27 10:38:40 -1000

    Third commit. Added the uppercase requirement feature.

--- password.js
+++ password.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const { isTypedArray } = require('util/types');
 
 const reader = readline.createInterface({
   input: process.stdin,
@@ -10,10 +11,26 @@ const reader = readline.createInterface({
 reader.question("Enter a password to validate:", function(input){
 	tokens = input.split(' ');
 	password = String(tokens[0]);
-	if (password.length >= 10){
+	let hasNumber = false;
+	let hasCapital = false;
+	let character = '';
+	for(let i = 0; i < password.length; i++){
+		if(!isNaN(input.charAt(i)) === true){
+			hasNumber = true;
+		}
+	}
+
+	for(let i = 0; i < password.length; i++){
+		character = password.charAt(i)
+		if(character === character.toUpperCase() && character !== character.toLowerCase()){
+			hasCapital = true;
+		}
+	}
+	
+	if (password.length >= 10 && hasNumber === true && hasCapital === true){
 		console.log("Success!")
 	}else{
-		console.log("Your password needs to be at least 10 characters and contain one uppercase character.")
+		console.log("Your password needs to be at least 10 characters, at least one number, and an uppercase letter.")
 	}
 
 	// This line closes the connection to the command line interface.
